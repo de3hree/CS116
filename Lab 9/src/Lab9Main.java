ce2bf1c09d195b97ab5349cc8475ca0f546b5555
Justin PMeyer <meyer177013@gmail.com>
2022-11-16 12:32:03 -0600

    Cleaned up the main
    Rather than each individual time being printed, the average is shown to
    make it legible. IDK if i need to change it or not

--- Lab 9/src/Lab9Main.java
+++ Lab 9/src/Lab9Main.java
@@ -1,111 +1,271 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Lab9Main {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> test1k = new ArrayList<Integer>(1000);
-		ArrayList<Integer> test2k = new ArrayList<Integer>(2000);
-		ArrayList<Integer> test4k =new ArrayList<Integer>(4000);
-		ArrayList<Integer> test8k = new ArrayList<Integer>(8000);
-		ArrayList<Integer> test16k = new ArrayList<Integer>(16000);
-		
-		ArrayList<Integer> test100 = new ArrayList<Integer>(100);
-		
-		ArrayList<Integer> testL = new ArrayList<Integer>();
-		testL.add(7);testL.add(2);testL.add(4);testL.add(3);testL.add(8);testL.add(5);testL.add(6);testL.add(10);
-		testL.add(1);testL.add(9);testL.add(8);
-		
-		
+
+		// Make zero filled ArrayLists of the appropriate sizes for 1k, 2k, 4k, 8k and
+		// 16k Integers
+		ArrayList<Integer> test1k = new ArrayList<Integer>(Collections.nCopies(1000, 0));
+		ArrayList<Integer> test2k = new ArrayList<Integer>(Collections.nCopies(2000, 0));
+		ArrayList<Integer> test4k = new ArrayList<Integer>(Collections.nCopies(4000, 0));
+		ArrayList<Integer> test8k = new ArrayList<Integer>(Collections.nCopies(8000, 0));
+		ArrayList<Integer> test16k = new ArrayList<Integer>(Collections.nCopies(16000, 0));
+
+		// Two longs, startTime to be overwritten on each sort call
+		// timeSum is to collect the execution time of each sort in milliseconds and
+		// then display it
+		long startTime = System.nanoTime();
+		long timeSum = 0;
+
+		// Var to control amount of iterations for each sort as well as the average
+		// calculation
+		int iterationNum = 10;
+
+		// Number generator for random integers to sort
 		Random rng = new Random();
-		
-		for(int i = 0; i < 100;i++) {
-			test1k.add(Integer.valueOf(rng.nextInt(-10000, 10001)));
-		}
-		
-		for(int i = 0; i < 1000;i++) {
-			test1k.add(Integer.valueOf(rng.nextInt(-10000, 10001)));
-		}
-		
-		for(int i = 0; i < 2000;i++) {
-			test2k.add(Integer.valueOf(rng.nextInt(-10000, 10001)));
-		}
-		
-		for(int i = 0; i < 4000;i++) {
-			test4k.add(Integer.valueOf(rng.nextInt(-10000, 10001)));
-		}
-		
-		for(int i = 0; i < 8000;i++) {
-			test8k.add(Integer.valueOf(rng.nextInt(-10000, 10001)));
-		}
-		
-		for(int i = 0; i < 16000;i++) {
-			test16k.add(Integer.valueOf(rng.nextInt(-10000, 10001)));
-		}
-		
-		//testBinaryInsertion(test1k);
-		//testInsertion(test1k);
-		//System.out.println("1k");
-		//testBinaryInsertion(test2k);
-		//testInsertion(test2k);
-		//System.out.println("2k");
-		//testBinaryInsertion(test4k);
-		//testInsertion(test4k);
-		//System.out.println("4k");
-		//testBinaryInsertion(test8k);
-		testInsertion(test8k);
-		//System.out.println("8k");
-		//testBinaryInsertion(test16k);
-		//testInsertion(test16k);
-		
-		
-		
-		
-	}
-	
-	
-	//I know that the intended way to show the time elapsed for each 
-	//   sort individually but using all 100 at the same time does kinda show the time difference as well
-	private static void testInsertion(ArrayList currArray) {
+
+		// Looping variable, overwritten on each while loop
 		int x = 0;
-		while(x < 100) {
-			long startTime = System.nanoTime();
-			ArrayList<Integer> iteration = currArray;
-			insertionSort(iteration);
-			x += 1;
+
+		// While loop that is repeated for each size of the array being tested, as well
+		// as a duplicate for binarySearches below
+		while (x < 100) {
+			// Generate the random numbers for each iteration
+			for (int i = 0; i < iterationNum; i++) {
+				test1k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+			// Start the timer
+			startTime = System.nanoTime();
+
+			// Call the relevant sorting method
+			insertionSort(test1k);
+
+			// End timer and calculate the difference
 			long endTime = System.nanoTime();
 			long timeElapsed = endTime - startTime;
-			System.out.println("Execution time in nanoseconds : " + timeElapsed);
-			System.out.println("Execution time in milliseconds : " + timeElapsed / 1000000);
-		}
-		
-		
-		
-	}
-	
-	private static void testBinaryInsertion(ArrayList currArray) {
-		int x = 0;
-		long startTime = System.nanoTime();
-		while(x < 100) {
-			binaryInsertionSort(currArray);
+
+			// Add time measurement to the timeSum
+			timeSum += timeElapsed / 1000000;
+
+			// Iterate loop
 			x += 1;
+
 		}
-		long endTime = System.nanoTime();
-		long timeElapsed = endTime - startTime;
-		System.out.println("Execution time in nanoseconds : " + timeElapsed);
-		System.out.println("Execution time in milliseconds : " + timeElapsed / 1000000);
+		// Print resulting average to console
+		System.out.println("1k average: (milli) " + (timeSum / iterationNum));
+
+		// Reset sum and looping variable for chaining tests
+		timeSum = 0;
+		x = 0;
+
+		// Repeat above for the remaining 4 insertion sorts
+
+		while (x < iterationNum) {
+			for (int i = 0; i < 2000; i++) {
+				test2k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+
+			startTime = System.nanoTime();
+			insertionSort(test2k);
+
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+			timeSum += timeElapsed / 1000000;
+			x += 1;
+
+		}
+
+		System.out.println("2k average: (milli) " + (timeSum / iterationNum));
+
+		timeSum = 0;
+		x = 0;
+
+		while (x < iterationNum) {
+			for (int i = 0; i < 4000; i++) {
+				test4k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+
+			startTime = System.nanoTime();
+			insertionSort(test4k);
+
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+			timeSum += timeElapsed / 1000000;
+			x += 1;
+
+		}
+
+		System.out.println("4k average: (milli) " + (timeSum / iterationNum));
+
+		timeSum = 0;
+		x = 0;
+
+		while (x < iterationNum) {
+			for (int i = 0; i < 8000; i++) {
+				test8k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+
+			startTime = System.nanoTime();
+			insertionSort(test8k);
+
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+			timeSum += timeElapsed / 1000000;
+			x += 1;
+
+		}
+
+		System.out.println("8k average: (milli) " + (timeSum / iterationNum));
+
+		timeSum = 0;
+		x = 0;
+
+		while (x < iterationNum) {
+			for (int i = 0; i < 16000; i++) {
+				test16k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+
+			startTime = System.nanoTime();
+			insertionSort(test16k);
+
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+			timeSum += timeElapsed / 1000000;
+			x += 1;
+
+		}
+
+		System.out.println("16k average: (milli) " + (timeSum / iterationNum));
+
+		timeSum = 0;
+		x = 0; 
 		
+		System.out.println("\nSTART OF BINARY SEARCH\n");
+
+		// Binary sorting testing, same as above with the method changed
+
+		while (x < 100) {
+			// Generate the random numbers for each iteration
+			for (int i = 0; i < iterationNum; i++) {
+				test1k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+			// Start the timer
+			startTime = System.nanoTime();
+
+			// Call the relevant sorting method
+			binaryInsertionSort(test1k);
+
+			// End timer and calculate the difference
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+
+			// Add time measurement to the timeSum
+			timeSum += timeElapsed / 1000000;
+
+			// Iterate loop
+			x += 1;
+
+		}
+		// Print resulting average to console
+		System.out.println("1k average: (milli) " + (timeSum / iterationNum));
+
+		// Reset sum and looping variable for chaining tests
+		timeSum = 0;
+		x = 0;
 		
+		while (x < iterationNum) {
+			for (int i = 0; i < 2000; i++) {
+				test2k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+
+			startTime = System.nanoTime();
+			binaryInsertionSort(test2k);
+
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+			timeSum += timeElapsed / 1000000;
+			x += 1;
+
+		}
+
+		System.out.println("2k average: (milli) " + (timeSum / iterationNum));
+
+		// Repeat above for the remaining 4 binary sorts
+
+		System.out.println("2k average: (milli) " + (timeSum / iterationNum));
+
+		timeSum = 0;
+		x = 0;
+
+		while (x < iterationNum) {
+			for (int i = 0; i < 4000; i++) {
+				test4k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+
+			startTime = System.nanoTime();
+			binaryInsertionSort(test4k);
+
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+			timeSum += timeElapsed / 1000000;
+			x += 1;
+
+		}
+
+		System.out.println("4k average: (milli) " + (timeSum / iterationNum));
+
+		timeSum = 0;
+		x = 0;
+
+		while (x < iterationNum) {
+			for (int i = 0; i < 8000; i++) {
+				test8k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+
+			startTime = System.nanoTime();
+			binaryInsertionSort(test8k);
+
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+			timeSum += timeElapsed / 1000000;
+			x += 1;
+
+		}
+
+		System.out.println("8k average: (milli) " + (timeSum / iterationNum));
+
+		timeSum = 0;
+		x = 0;
+
+		while (x < iterationNum) {
+			for (int i = 0; i < 16000; i++) {
+				test16k.set(i, Integer.valueOf(rng.nextInt(-10000, 10001)));
+			}
+
+			startTime = System.nanoTime();
+			binaryInsertionSort(test16k);
+
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+			timeSum += timeElapsed / 1000000;
+			x += 1;
+
+		}
+
+		System.out.println("16k average: (milli) " + (timeSum / iterationNum));
+
 	}
-	
-	
+
 	public static void insertionSort(ArrayList<Integer> inputArr) {
 		int temp = 0;
-		for(int x = 1;x < inputArr.size();x++) {
-			for(int y = x-1; y >= 0; y--) {
-				if(inputArr.get(y) > inputArr.get(x)) {
+		for (int x = 1; x < inputArr.size(); x++) {
+			for (int y = x - 1; y >= 0; y--) {
+				if (inputArr.get(y) > inputArr.get(x)) {
 					temp = inputArr.get(x);
 					inputArr.set(x, inputArr.get(y));
 					inputArr.set(y, temp);
@@ -114,17 +274,17 @@ public class Lab9Main {
 			}
 		}
 	}
-	
+
 	public static void binaryInsertionSort(ArrayList<Integer> inputArr) {
-		for(int x = 1;x < inputArr.size();x++) {
-			if(inputArr.get(x) < inputArr.get(x-1)) {
+		for (int x = 1; x < inputArr.size(); x++) {
+			if (inputArr.get(x) < inputArr.get(x - 1)) {
 				int high = x;
 				int low = 0;
 				int mid = (high + low) / 2;
-				while(high >= low) {
-					if(inputArr.get(x) < inputArr.get(mid)) {
+				while (high >= low) {
+					if (inputArr.get(x) < inputArr.get(mid)) {
 						high = mid - 1;
-					} else if(inputArr.get(x) >= inputArr.get(mid)) {
+					} else if (inputArr.get(x) >= inputArr.get(mid)) {
 						low = mid + 1;
 					}
 					mid = (high + low) / 2;
